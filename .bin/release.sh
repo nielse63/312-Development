00d3a4faca7f1962c4fe3f3f5fec45c4ba8870c7
Erik Nielsen <erik@312development.com>
2016-07-26 06:09:36 -0500

    Removing app cache declaration

--- .bin/release.sh
+++ .bin/release.sh
@@ -5,20 +5,7 @@ read -p "Enter a commit message: " answer
 if [ "$answer" = '' ] ; then
 	answer="New release"
 fi
-git add -A && git commit -m "$answer";
-git push heroku master && heroku open;
+git add -A;
+git commit -m "$answer";
 git push origin master;
-
-# read year
-
-# if (( ("$year" % 400) == "0" )) || (( ("$year" % 4 == "0") && ("$year" % 100 !=
-# "0") )); then
-#   echo "$year is a leap year."
-# else
-#   echo "This is not a leap year."
-# fi
-
-# michel ~/test> leaptest.sh
-# Type the year that you want to check (4 digits), followed by [ENTER]:
-# 2000
-# 2000 is a leap year.
+git push heroku master;
