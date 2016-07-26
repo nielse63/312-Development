38fa2b88b7bc5173f2b468dd54dec3e8118ded74
Erik Nielsen <erik@312development.com>
2016-07-26 06:02:31 -0500

    Including `.bin` dir for easy script management

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -218,7 +218,11 @@ class AppUI {
 	}
 
 	registerServiceWorker() {
-		if ('serviceWorker' in navigator) {
+		var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+			window.location.hostname === '[::1]' ||
+			window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+		);
+		if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost) ) {
 			navigator.serviceWorker.register('/sw.js', {
 				scope: '/'
 			});
