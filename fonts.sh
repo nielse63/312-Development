2156ed84a307b096db2d69ebeb38333b049a88bf
Erik Nielsen <erik@312development.com>
2016-07-29 01:15:11 -0500

    Removed unneeded font files and updated font-face declarations;

--- fonts.sh
+++ fonts.sh
@@ -5,10 +5,9 @@ do
 	# vars
 	BASENAME=${f##*[/|\\]}
 	NO_EXTENSION=${BASENAME%.ttf}
-	DIR=$(dirname $f)
-	EOT_NAME="$DIR/$NO_EXTENSION.eot"
-	WOFF_NAME="$DIR/$NO_EXTENSION.woff"
-	WOFF2_NAME="$DIR/$NO_EXTENSION.woff2"
+	IN_DIR=$(dirname $f)
+	WOFF_NAME="$IN_DIR/$NO_EXTENSION.woff"
+	WOFF2_NAME="$IN_DIR/$NO_EXTENSION.woff2"
 
 	# convert to compressed woff
 	if [ ! -f ${WOFF_NAME} ] ; then
@@ -18,11 +17,6 @@ do
 	# convert to compressed woff2
 	if [ ! -f ${WOFF2_NAME} ] ; then
 		woff2_compress ${f};
-	fi
-
-	# convert to compressed eot
-	if [ ! -f ${EOT_NAME} ] ; then
-		ttf2eot < ${f} > ${EOT_NAME};
 	fi
 
 	# notify
