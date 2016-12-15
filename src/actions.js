53a89c5cb4891182bc2800a6f024fbf636f143ef
Erik Nielsen <erik@312development.com>
2016-12-15 05:59:21 -0600

    Running new eslint config

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,14 @@
 
 export function addTitle(text) {
-	return {
-		type: 'ADD_TITLE',
-		text
-	};
+  return {
+    type: 'ADD_TITLE',
+    text,
+  }
 }
 
 export function addDescription(text) {
-	return {
-		type: 'ADD_DESCRIPTION',
-		text
-	};
+  return {
+    type: 'ADD_DESCRIPTION',
+    text,
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 
-import { h, render } from 'preact';
-import style from './style/index.scss';
+import { h, render } from 'preact'
+import style from './style/index.scss'
 
-let root;
+let root
 function init() {
-	const App = require('./components/app').default;
-	root = render(
+  const App = require('./components/app').default
+  root = render(
     <App
       cssclass={style.main}
       readyclass={style.ready}
       doneclass={style.complete}
       loadingclass={style.loading}
     />,
-    document.body, root
-  );
+    document.body, root,
+  )
 }
 
-init();
+init()
 
 if (module.hot) {
-	require('preact/devtools');
-	module.hot.accept('./components/app', () => requestAnimationFrame(init));
+  require('preact/devtools')
+  module.hot.accept('./components/app', () => requestAnimationFrame(init))
 }
