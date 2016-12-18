73087381d2cf90efaac9aade4ba2407b6e08ea93
Erik Nielsen <erik@312development.com>
2016-12-18 12:24:35 -0600

    Update protocol transmission

---
+++ test/.eslintrc.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  "env": {
+    "node": true,
+    "mocha": true,
+    "es6": true
+  },
+  "rules": {
+    "max-nested-callbacks": ["error", 4],
+    "no-unused-expressions": "off"
+  }
+};
