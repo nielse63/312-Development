b85c3924c1f4548e1a6497bbb2a89fc03db227a0
Erik Nielsen <erik@312development.com>
2017-01-12 05:18:37 -0600

    Update package

--- pm2.js
+++ pm2.js
@@ -2,7 +2,7 @@
 
 const pm2 = require('pm2')
 
-const instances = process.env.WEB_CONCURRENCY || 4
+const instances = process.env.WEB_CONCURRENCY || 1
 const maxMemory = process.env.WEB_MEMORY || 512
 const ENV = process.env.NODE_ENV || 'development'
 
