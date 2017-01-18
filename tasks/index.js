9c7b8dc4fa30dfcc6969072141550dee37c4e590
Erik Nielsen <erik@312development.com>
2017-01-18 06:20:22 -0600

    Simplify task runner

--- tasks/index.js
+++ tasks/index.js
@@ -1,34 +1,18 @@
 /* eslint-disable no-console */
 
-const async = require('async')
 const css = require('./critical-css')
 const fonts = require('./fonts')
 const images = require('./images')
 
-const q = async.queue((task, callback) => {
-  console.log(`Running task:${task.name}`)
-  return new Promise(resolve => {
-    task.function(resolve)
-  }).then(() => {
-    callback()
-  }).catch(err => {
-    throw err
-  })
-}, 3)
-
-q.push([
-  {
-    name: 'css',
-    function: css,
-  }, {
-    name: 'fonts',
-    function: fonts,
-  }, {
-    name: 'images',
-    function: images,
-  },
-], err => {
-  if (err) {
-    throw err
-  }
+console.log('Running task: css')
+css(() => {
+  console.log('Complete: css')
+})
+console.log('Running task: fonts')
+fonts(() => {
+  console.log('Complete: fonts')
+})
+console.log('Running task: images')
+images(() => {
+  console.log('Complete: images')
 })
