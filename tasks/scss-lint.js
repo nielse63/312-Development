b62e8399a3cdfaa99aa1a1a1c78684aa9c7a1a39
Erik Nielsen <erik@312development.com>
2017-04-04 03:31:10 -0500

    Updated scss-autofix script

--- tasks/scss-lint.js
+++ tasks/scss-lint.js
@@ -1,25 +1,104 @@
 
-// const shelljs = require('shelljs')
-// const cp = require('child_process')
-// const path = require('path')
-// const fs = require('fs')
+const shelljs = require('shelljs')
+const cp = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const namer = require('color-namer')
 
-// const src = path.join(__dirname, '../src')
-// // const env = Object.assign({
-// //   PATH: '/Users/enielsen/.gem/ruby/2.3.3/bin:/Users/enielsen/.rvm/rubies/ruby-2.3.3/lib/ruby/gems/2.3.0/bin:/Users/enielsen/.rubies/ruby-2.3.3/bin:/Users/enielsen/.rvm/gems/ruby-2.3.1@global/bin:/Users/enielsen/.rvm/rubies/ruby-2.3.1/bin:/Users/enielsen/.gem/ruby/2.3.1/bin:/Users/enielsen/Sites/vm/8b/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/go/bin:/Applications/Wireshark.app/Contents/MacOS:/usr/local/go/bin:/Users/enielsen/go/bin:/usr/local/go/bin:/Users/enielsen/go/bin:/usr/local/bin:/usr/local/Cellar/node/7.7.2/bin:/usr/local/go/bin:/Users/enielsen/go/bin:/usr/local/go/bin:/Users/enielsen/go/bin:/Users/enielsen/.rvm/bin'
-// // }, process.env)
+const src = path.join(__dirname, '../src')
+const env = Object.assign({
+  PATH: [
+    '/Users/enielsen/.gem/ruby/2.3.3/bin',
+    '/Users/enielsen/.rvm/rubies/ruby-2.3.3/lib/ruby/gems/2.3.0/bin',
+    '/Users/enielsen/.rubies/ruby-2.3.3/bin',
+    '/Users/enielsen/.rvm/gems/ruby-2.3.1/bin',
+    '/Users/enielsen/.rvm/gems/ruby-2.3.1@global/bin',
+    '/Users/enielsen/.rvm/rubies/ruby-2.3.1/bin',
+    '/Users/enielsen/Sites/vm/8b/bin',
+    '/usr/local/opt/gettext/bin',
+    '/usr/local/opt/findutils/libexec/gnubin',
+    '/usr/local/bin',
+    '/usr/local/sbin',
+    '/usr/local/bin',
+    '/usr/bin',
+    '/bin',
+    '/usr/sbin',
+    '/sbin',
+    '/opt/X11/bin',
+    '/usr/local/go/bin',
+    '/Applications/Wireshark.app/Contents/MacOS',
+    '/usr/local/go/bin',
+    '/Users/enielsen/go/bin',
+    '/Users/enielsen/.rvm/bin',
+  ].join(':')
+}, process.env)
 
-// shelljs.exec(`scss-lint --format=JSON ${src}`, {
-//   async: true,
-//   silent: true,
-//   // cwd: path.join(__dirname, '../'),
-//   // env: env,
-// }, (err, stdout, stderr) => {
-//   // if(err) {
-//   //   console.log(err)
-//   //   console.log(stderr)
-//   //   return
-//   // }
-//   const results = JSON.parse(stdout)
-//   console.log(results) // eslint-disable-line no-console
-// })
+function handleColorVars(file, results) {
+  const colors = [...new Set(results.filter(function(object) {
+    return object.linter === 'ColorVariable'
+  }).map(function(object) {
+    const reason = object.reason.match(/`(.*?)`/)
+    return (reason && reason[1]) || ''
+  }).sort())]
+  let fileContent = fs.readFileSync(file, 'utf8')
+  if(!colors.length) {
+    return fileContent
+  }
+  const colorVars = []
+  const colorNames = []
+  colors.forEach(function(color, i) {
+    const rgx = new RegExp(color, 'g')
+    const names = namer(color)
+    let name = names.basic[0].name
+    if(colorNames.indexOf(name) > -1) {
+      const count = colorNames.filter(function(c) {
+        return c === name
+      }).length
+      name = `${name}${count + 1}`
+    }
+    if(fileContent.indexOf(`$${name};`) > -1) {
+      name = `${name}Custom`
+    }
+    colorNames.push(name)
+    colorVars.push(`$${name}: ${color};`)
+    fileContent = fileContent.replace(rgx, `$${name}`)
+  })
+
+  const masthead = `
+// Generated Color Variables
+// ========================================================================== */
+
+${colorVars.join('\n')}
+`
+  return [masthead, fileContent].join('\n')
+}
+
+function resultsForFile(file, results) {
+  const types = results.map(function(object) {
+    return object.linter
+  })
+  let fileContent = ''
+
+  // handle color vars
+  if(types.indexOf('ColorVariable') > -1) {
+    fileContent = handleColorVars(file, results)
+  }
+
+  // write out file
+  if(fileContent) {
+    fs.writeFileSync(file, fileContent)
+  }
+}
+
+shelljs.exec(`scss-lint --format=JSON ${src}`, {
+  async: true,
+  silent: true,
+  cwd: path.join(__dirname, '../'),
+  env: env,
+}, (err, stdout, stderr) => {
+  const results = JSON.parse(stdout)
+  Object.keys(results).forEach(function(file) {
+    const result = results[file]
+    resultsForFile(file, result)
+  })
+})
