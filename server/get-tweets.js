463f42984bcc90fcc1db02bde40c3ec21d0c467b
Erik Nielsen <erik@312development.com>
2018-03-15 16:52:37 -0500

    updated build scripts and config

--- server/get-tweets.js
+++ server/get-tweets.js
@@ -1,8 +1,35 @@
 
 const { Twitter } = require('twitter-node-client');
-const { loadENV } = require('./helpers');
+const { format, isToday, isYesterday } = require('date-fns');
 
-loadENV();
+const formatDate = (createdAt) => {
+  const date = new Date(createdAt);
+  const time = format(date, 'h:mm a');
+  if (isToday(date)) {
+    return `Today @ ${time}`;
+  }
+  if (isYesterday(date)) {
+    return `Yesterday @ ${time}`;
+  }
+  return format(date, 'MMM DD @ HH:mm');
+};
+
+const formatTweet = (object) => {
+  const { id, text } = object;
+  const screenName = object.user.screen_name;
+  const time = object.created_at;
+  const date = formatDate(time);
+  const url = `https://twitter.com/${screenName}/status/${id}`;
+
+  return {
+    url, text, date, time,
+  };
+};
+
+const formatTweets = (data) => {
+  const array = typeof data === 'string' ? JSON.parse(data) : data;
+  return array.map(formatTweet);
+};
 
 module.exports = function getTweets() {
   return new Promise(((resolve, reject) => {
@@ -13,12 +40,11 @@ module.exports = function getTweets() {
       accessTokenSecret: process.env.TWITTER_ACCESS_SECRET,
       callBackUrl: 'https://312development.com/twitter-oath',
     });
-    twitter.getUserTimeline({ screen_name: process.env.TWITTER_USERNAME, count: 3 }, (error) => {
+    twitter.getUserTimeline({ screen_name: process.env.TWITTER_USERNAME, count: '3' }, (error) => {
       reject(error);
     }, (success) => {
-      const output = typeof success === 'string' ?
-        JSON.parse(success) : success;
-      resolve(output);
+      const tweets = formatTweets(success);
+      resolve(tweets);
     });
   })).catch((error) => {
     console.error(error);
