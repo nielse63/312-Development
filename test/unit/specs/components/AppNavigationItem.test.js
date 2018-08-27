33c2c03fa94f056d97441578c6d3b1d7617badfd
Erik Nielsen <erik@312development.com>
2018-08-27 09:41:10 -0500

    :white_check_mark: (unit) updated unit tests pertinent to app navigation

--- test/unit/specs/components/AppNavigationItem.test.js
+++ test/unit/specs/components/AppNavigationItem.test.js
@@ -2,13 +2,45 @@ import { shallowMount } from '@vue/test-utils';
 import AppNavigationItem from '@/components/AppNavigationItem';
 
 describe('AppNavigationItem.vue', () => {
-  it('renders correctly', () => {
-    const wrapper = shallowMount(AppNavigationItem, {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(AppNavigationItem, {
       propsData: {
-        path: '/',
-        text: 'Home',
+        path:  '/#about-me',
+        text:  'About Me',
+        color: 'purple',
       },
+      attachToDocument: true,
     });
+  });
+
+  it('renders correctly', () => {
     expect(wrapper.contains('li')).toBe(true);
+  });
+
+  it('should set properties', () => {
+    const link = wrapper.find('.purple');
+    expect(link.is('a')).toBeTruthy();
+    expect(link.classes()).toContain('purple');
+    expect(link.attributes().href).toBe('/#about-me');
+    expect(link.text()).toBe('About Me');
+  });
+
+  it('should emit `navclose` event when no href sent', (done) => {
+    wrapper.setProps({ path: '/' });
+    wrapper.find('a').trigger('click');
+    setTimeout(() => {
+      expect(wrapper.emitted('navclose')).toBeTruthy();
+      done();
+    }, 1000);
+  });
+
+  it('should not emit `navclose` event when target not found', (done) => {
+    wrapper.find('a').trigger('click');
+    setTimeout(() => {
+      expect(wrapper.emitted('navclose')).toBeFalsy();
+      done();
+    }, 1000);
   });
 });
